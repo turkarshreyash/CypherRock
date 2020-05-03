45a93dae4fbeeb858ba83a5ff9ef6540cd886edd
Shreyash7478 <shturkar90@gmail.com>
2020-05-03 15:49:57 +0530

    standards sizes

---
+++ standardsize.h
@@ -0,0 +1,19 @@
+/*
+This file contains all the standard feild sizes
+*/
+
+#pragma once
+
+
+//length in bytes
+
+#define VERSION_LEN 4
+#define NUMBER_OF_INPUTS_LEN 1
+#define PREV_TXN_HASH_LEN 32
+#define PREV_OUTPUT_INDEX_LEN 4
+#define SCRIPT_LENGTH_LEN 1
+#define SEQ_LEN 4
+#define NUMBER_OF_OUTPUTS_LEN 1
+#define VALUE_LEN 8
+#define LOCKTIME_LEN 4
+#define SIG_HASH_CODE_LEN 4
